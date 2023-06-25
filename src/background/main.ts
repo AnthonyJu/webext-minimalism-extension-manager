b73b86725c16af1739a8d6e23ac35447706444d7
JuPeng <1662345805@qq.com>
2023-06-25 23:58:03 +0800

    fix：修复在相同规则下更换地址导致扩展不好用问题

--- src/background/main.ts
+++ src/background/main.ts
@@ -10,24 +10,25 @@ interface TabWithExts extends browser.Tabs.Tab {
 }
 
 // 保存所有打开的标签页
-let allOpenTabs: TabWithExts[] = []
+const allOpenTabs: TabWithExts[] = []
 
-// 监听标签页的更新以及开启
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status !== 'complete' && changeInfo.url) {
-    // 判断是否是新开启的标签页
-    const replacedTab = allOpenTabs.find(tab => tab.id === tabId)
-    // 如果不是新开启的标签页,从allOpenTabs中移除更新的标签页
-    if (replacedTab) setExtEnabled(replacedTab, false)
-    // 不管是新开启还是更新的标签页都要重新设置扩展的启用状态
-    setExtEnabled(tab, true)
-  }
-})
 // 监听标签页的关闭
 browser.tabs.onRemoved.addListener((tabId) => {
   // 获取关闭的标签页
-  const removedTab = allOpenTabs.find(tab => tab.id === tabId)
+  const removedTab = allOpenTabs.find(openTab => openTab.id === tabId)
   if (removedTab) setExtEnabled(removedTab, false)
+})
+
+// 监听标签页的更新以及开启
+browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'loading' && changeInfo.url) {
+    // 判断是否是新开启的标签页
+    const replacedTab = allOpenTabs.find(openTab => openTab.id === tabId)
+    // 不管是新开启还是更新的标签页都要重新设置扩展的启用状态（需要优先处理）
+    await setExtEnabled(tab, true)
+    // 如果不是新开启的标签页, 则关闭之前的标签页使用的扩展
+    if (replacedTab) setExtEnabled(replacedTab, false)
+  }
 })
 
 // 根据规则设置扩展的启用状态
@@ -54,16 +55,22 @@ async function setExtEnabled(tab: TabWithExts, newTab: boolean) {
       if (isMatch) exts.push(...rule.extIds)
     })
 
-    // 保存当前标签页
-    allOpenTabs.push({ ...tab, exts: [...new Set(exts)] })
     // 循环开启标签页使用的扩展
     exts!.forEach((extId) => {
-      browser.management.setEnabled(extId, true)
+      // 判断其他标签页是否已经使用该扩展
+      const hasOtherMatch = allOpenTabs.some((otherTab) => {
+        return otherTab.exts!.includes(extId)
+      })
+      // 如果没有其他标签页使用该扩展则开启
+      if (!hasOtherMatch) browser.management.setEnabled(extId, true)
     })
+
+    // 保存当前标签页
+    allOpenTabs.push({ ...tab, exts: [...new Set(exts)] })
   }
   else {
-    // 从allOpenTabs中移除关闭的标签页
-    allOpenTabs = allOpenTabs.filter(openTab => openTab.id !== tab.id)
+    // 从allOpenTabs中移除标签页
+    allOpenTabs.splice(allOpenTabs.indexOf(tab), 1)
 
     // 循环关闭标签页的扩展
     tab.exts!.forEach((extId) => {
